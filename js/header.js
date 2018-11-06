aacb154ca234e3ddbdf60e7d78b39a7d8f4c3065
WaleedIqbal <waleed.iqbal@now.net.pk>
2018-11-06 12:27:00 +0500

    Removed unnecessary white line.

--- js/header.js
+++ js/header.js
@@ -1,10 +1,5 @@
-
-
-(function(){
-    $("#HomeSideLinksList span").click(function(e){
-        this.children[0].click();
-    });
-}());
-
-
-
+(function () {
+    $("#HomeSideLinksList span").click(function (e) {
+        this.children[0].click();
+    });
+}());
